2d515421b9249361074d458efd0e7fa5065f76e5
Brendan Samek <brendan@brendansamek.com>
2025-05-29 10:30:45 -0600

    use flying-buffalo domain for nextjs

--- next.config.mjs
+++ next.config.mjs
@@ -51,8 +51,8 @@ const nextConfig = {
       {
         source: "/:path*",
         // TODO: switch this over on webflow to be the main page that other things redirect to.
-        // destination: "https://flying-buffalo.buildcanada.com/:path*",
-        destination: "https://www.buildcanada.com/:path*",
+        destination: "https://flying-buffalo.buildcanada.com/:path*",
+        // destination: "https://www.buildcanada.com/:path*",
       },
     ];
   },
