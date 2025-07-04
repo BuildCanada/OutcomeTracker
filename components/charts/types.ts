c761b36fbc751969a4af996bf586c3ae8f7f6440
Razvan Marescu <razvan@marescu.net>
2025-07-04 16:40:22 -0700

    f

--- components/charts/types.ts
+++ components/charts/types.ts
@@ -1,23 +1,9 @@
 
+import type { ChartDataset } from "chart.js";
+
 /**
- * Configuration interface for Chart.js line chart datasets.
+ * Configuration type for Chart.js line chart datasets.
  * 
  * @see {@link https://www.chartjs.org/docs/latest/charts/line.html | Chart.js Line Chart Documentation}
  */
-export interface LineChartDataset {
-  backgroundColor: string;
-  label: string;
-  data: (number | null)[];
-  borderColor: string;
-  tension?: number;
-  borderWidth?: number;
-  pointRadius?: number;
-  pointHoverRadius?: number;
-  borderDash?: number[];
-  fill?: boolean | string;
-  stack?: string;
-  order?: number;
-  type?: any;
-  yAxisID?: string;
-  [key: string]: any;
-}
+export type LineChartDataset = ChartDataset<"line", (number | null)[]>;
